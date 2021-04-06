4605c31a9fa10b14b9f7d4d06354ad12fb09425a
parimala-k <77636879+parimala-k@users.noreply.github.com>
2021-04-06 16:05:22 -0700

    data pre-processing

---
+++ SWM-G13-P21.R
@@ -0,0 +1,31 @@
+# Read in files
+users <- read.csv(file = "C:/Users/pkondur1/OneDrive - Arizona State University/Desktop/SWM/sample_dataset/users.csv")
+likes <- read.csv(file = "C:/Users/pkondur1/OneDrive - Arizona State University/Desktop/SWM/sample_dataset/likes.csv")
+users_likes <- read.csv(file = "C:/Users/pkondur1/OneDrive - Arizona State University/Desktop/SWM/sample_dataset/users-likes.csv")
+
+# Match entries in users_likes with users and likes dictionaries
+users_likes$user_row<-match(users_likes$userid,users$userid)
+users_likes$like_row<-match(users_likes$likeid,likes$likeid)
+
+install.packages("Matrix")
+
+# Load Matrix library
+require(Matrix)
+
+# Construct the sparse User-Like(user footprint) Matrix ufp
+ufp <- sparseMatrix(i = users_likes$user_row, j = users_likes$like_row, x = 1)
+
+# Save user IDs as row names in M
+rownames(ufp) <- users$userid
+
+# Save Like names as column names in M
+colnames(ufp) <- likes$name
+
+# Retain only these rows/columns that meet the threshold
+repeat {                                       
+  i <- sum(dim(ufp))                             
+  ufp <- ufp[rowSums(ufp) >= 50, colSums(ufp) >= 150] 
+  if (sum(dim(ufp)) == i) break                  
+}
+
+users <- users[match(rownames(ufp), users$userid), ]
